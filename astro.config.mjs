3c875c00e708334dd8619178dc3407354409dba7
matthewgoodyear <mattgdyr@proton.me>
2024-03-28 11:10:13 +0000

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://matthewgoodyear.github.io',
   base: 'astro-playground',
+  prefetch: true,
+  integrations: [react()],
 });
